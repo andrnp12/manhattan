8fa21629b6977dd3bd37caf30520215457756e3e
andrnp12 <andre.diko46@gmail.com>
2025-09-16 19:04:23 +0700

    semifinal index

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         style="background-image: url(images/slider-image.jpg);">
         <div class="container m-auto">
           <div class="text-start col-md-10">
-            <h2 class="display-1 fw-bold text-white">Top Solar and Renewable Energy</h2>
+            <h2 class="display-1 fw-bold text-white">Materi Ajar RPS Terbaik dan Terpadu</h2>
             <p class="text-white">Vero elitr justo clita dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
             <a href="services-single.html" class="btn btn-primary mt-3">Read More</a>
           </div>
@@ -112,7 +112,7 @@
       <div class="row align-items-center">
         <div class="col-lg-6">
           <div class="imageblock me-4 position-relative">
-            <img class="img-fluid" src="images/about.png" alt="img">
+            <img class="img-fluid" src="images/materi.jpg" alt="img">
             <a type="button" data-bs-toggle="modal" data-src="https://www.youtube.com/embed/W_tIumKa8VY"
               data-bs-target="#myModal" class="play-btn position-absolute top-50 start-50 translate-middle">
               <svg class="play-icon text-primary" width="70" height="70">
@@ -122,13 +122,13 @@
           </div>
         </div>
         <div class="col-lg-6 mt-5 mt-lg-0">
-          <h6 class="text-primary">About Us</h6>
-          <h3 class="display-5 fw-bold mb-3">Among the Top 10 Solar & Renewable Energy Industry</h3>
+          <h6 class="text-primary">Tentang Kami</h6>
+          <h3 class="display-5 fw-bold mb-3">Situs Edukasi Bahan Ajar RPS dan Materi Pendidikan Terbaik</h3>
           <p>Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo erat amet. Tempor erat sed stet lorem
             sit clita duo justo elitr rebum at clita diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat
             ipsum et lorem et sit, sed stet lorem sit clita duo justo erat amet
           </p>
-          <a class="btn btn-primary mt-4" href="about.html">About Us</a>
+          <a class="btn btn-primary mt-4" href="about.html">Jelajahi Sekarang</a>
         </div>
       </div>
     </div>
@@ -136,29 +136,17 @@
 
   <section id="services">
     <div class="container-fluid p-0">
-      <h6 class="text-center text-primary">Our Services</h6>
-      <h3 class="text-center display-5 fw-bold mb-3">best services from us</h3>
-      <div class="row g-0 mt-5">
+      <h6 class="text-center text-primary">Layanan Kami</h6>
+      <h3 class="text-center display-5 fw-bold mb-3">Apa Yang Kami Tawarkan</h3>
+      <div class="row g-0 mt-5 justify-content-center">
         <div class="col-md-6 col-lg-3">
           <div class="service-post position-relative bg-primary">
-            <img src="images/service3.jpg" class="service-img img-fluid" alt="img">
+            <img src="images/service1.jpg" class="service-img img-fluid" alt="img">
             <div class="position-absolute bottom-0 p-5">
-              <h5 class="text-white">Wind turbines</h5>
+              <h5 class="text-white">Video Pembelajaran Edukatif</h5>
               <p class="text-white">Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
               <a href="services-single.html">
-                <h6 class="text-white text-decoration-underline">Read more</h6>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3">
-          <div class="service-post position-relative bg-primary">
-            <img src="images/service4.jpg" class="service-img img-fluid" alt="img">
-            <div class="position-absolute bottom-0 p-5">
-              <h5 class="text-white">Solar power systems</h5>
-              <p class="text-white">Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
-              <a href="services-single.html">
-                <h6 class="text-white text-decoration-underline">Read more</h6>
+                <h6 class="text-white text-decoration-underline">Lihat Semua</h6>
               </a>
             </div>
           </div>
@@ -167,15 +155,27 @@
           <div class="service-post position-relative bg-primary">
             <img src="images/service2.jpg" class="service-img img-fluid" alt="img">
             <div class="position-absolute bottom-0 p-5">
-              <h5 class="text-white">Hydropower plants</h5>
+              <h5 class="text-white">Materi Ajar RPS Terupdate</h5>
               <p class="text-white">Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
               <a href="services-single.html">
-                <h6 class="text-white text-decoration-underline">Read more</h6>
+                <h6 class="text-white text-decoration-underline">Lihat Semua</h6>
               </a>
             </div>
           </div>
         </div>
         <div class="col-md-6 col-lg-3">
+          <div class="service-post position-relative bg-primary">
+            <img src="images/service3.jpg" class="service-img img-fluid" alt="img">
+            <div class="position-absolute bottom-0 p-5">
+              <h5 class="text-white">Video dan Materi Downloadable</h5>
+              <p class="text-white">Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
+              <a href="services-single.html">
+                <h6 class="text-white text-decoration-underline">Lihat Semua</h6>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="col-md-6 col-lg-3">
           <div class="service-post position-relative bg-primary">
             <img src="images/service1.jpg" class="service-img img-fluid" alt="img">
             <div class="position-absolute bottom-0 p-5">
@@ -186,12 +186,12 @@
               </a>
             </div>
           </div>
-        </div>
+        </div> -->
       </div>
     </div>
   </section>
 
-  <section id="feature" class="padding-medium">
+  <!-- <section id="feature" class="padding-medium">
     <div class="container">
       <h6 class="text-primary">Why Choose Us!</h6>
       <h3 class="display-5 fw-bold mb-3">Our motive to change world</h3>
@@ -224,9 +224,9 @@
         </div>
       </div>
     </div>
-  </section>
+  </section> -->
 
-  <section id="projects">
+  <section id="projects" class="padding-medium">
     <div class="container">
       <h6 class="text-center text-primary">Our Projects</h6>
       <h3 class="text-center display-5 fw-bold ">View Our Latest Projects</h3>
@@ -343,7 +343,7 @@
 
   </section> -->
 
-  <section id="blogs" class="padding-medium">
+  <!-- <section id="blogs">
     <div class="container">
       <div class="mb-3 d-flex flex-wrap align-items-center justify-content-between">
         <div>
@@ -370,7 +370,7 @@
         </div>
       </div>
     </div>
-  </section>
+  </section> -->
 
   <section id="contact-info" class="padding-small bg-gray">
     <div class="container">
